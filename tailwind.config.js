2e37c52547ee7c81f117077a975d9498c95ca66d
Patrick Serrano <patrickcmserrano@gmail.com>
2026-01-03 00:30:36 -0300

    style: fix layout issues, improve visibility and responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ['class', '[data-mode="dark"]'],
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: [
+    './src/**/*.{html,js,svelte,ts}',
+    './src/lib/**/*.{html,js,svelte,ts}',
+  ],
   theme: {
     extend: {},
   },
